e465a72bd763bdddc26d25c9ee60e0b54d4a7c90
jay77i <jxy45@aol.com>
2024-03-14 14:34:07 +0000

    Removed useless vars

--- src/main/java/org/jay/Settings.java
+++ src/main/java/org/jay/Settings.java
@@ -6,9 +6,7 @@ public class Settings {
 
     public static String clientUrl = "https://github.com/jay77i/Unofficial-EradX-Launcher/raw/main/.bin/EradicationX.jar";
     public static String clientPath = "C:\\Users\\" + System.getenv("USERNAME") + "\\EXPP.dat";
-    public static String cachePath = "C:\\Users\\" + System.getenv("USERNAME") + "\\PPcache";
 
     public static String JvmArgs = "--add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.desktop/java.beans=ALL-UNNAMED -jar";
-    public static String JarArgs = "";
 
 }
